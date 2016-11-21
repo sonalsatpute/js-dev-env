5be911bc3fda023466336e466777fa9468fa6523
Sonal Satpute <sonal.satpute@gmail.com>
2016-11-21 20:35:30 +0530

    added http call

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1, "firstName": "Sonal", "lastName": "Satpute", "email": "sonal@readgreenclean.com"},
+    {"id": 2, "firstName": "Saurabh", "lastName": "Mahajan", "email": "saurabh@readgreanclean.com"},
+    {"id": 3, "firstName": "Swapnil", "lastName": "Khare", "email": "swapnil@readgreenclean.com"}
+  ]);
+});
+
 app.listen(port, function (err){
   if (err){
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -3,17 +3,17 @@ import jsdom from 'jsdom';
 import fs from 'fs';
 
 describe('Our first test', () => {
-  it('should pass', () => {
+  it('should pass',  () => {
     expect(true).to.equal(true);
   });
 });
 
 describe('index.html', () => {
-  it('should say hello world!', (done) => {
+  it('should have h1 that syas Users', (done) => {
      const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function (err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('Hello World!');
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     });
